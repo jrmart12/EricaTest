a99ab3aad9ff57ce1a1fbea0b996e03575354e59
jrmart12 <rodolfojose@live.com>
2021-11-02 11:13:28 -0600

    whatever you want

---
+++ javascript.js
@@ -0,0 +1,27 @@
+//let, var, const
+
+var x = 1;
+let y = 1;
+
+const hello = 'world';
+
+console.log(x + y);
+console.log(hello);
+
+function freezer() {
+  let sandwich;
+  sandwich = ['bread', 'ham', 'cheese', 'bread'];
+  //sandwich.push('ketchup');
+  console.log(sandwich.indexOf('ham'));
+  console.log(sandwich);
+}
+
+freezer();
+
+let type_sandwich = {
+  bread: 'whole bread',
+  ham: 'turkey',
+  cheese: 'swiss',
+};
+
+console.log(type_sandwich.bread);
