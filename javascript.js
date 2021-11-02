0af9bc6eb4b1d0a74bb6cee278cf6f2809d500bb
jrmart12 <rodolfojose@live.com>
2021-11-02 11:20:17 -0600

    changed world to w

--- javascript.js
+++ javascript.js
@@ -3,25 +3,25 @@
 var x = 1;
 let y = 1;
 
-const hello = 'world';
+const hello = "w";
 
 console.log(x + y);
 console.log(hello);
 
 function freezer() {
   let sandwich;
-  sandwich = ['bread', 'ham', 'cheese', 'bread'];
+  sandwich = ["bread", "ham", "cheese", "bread"];
   //sandwich.push('ketchup');
-  console.log(sandwich.indexOf('ham'));
+  console.log(sandwich.indexOf("ham"));
   console.log(sandwich);
 }
 
 freezer();
 
 let type_sandwich = {
-  bread: 'whole bread',
-  ham: 'turkey',
-  cheese: 'swiss',
+  bread: "whole bread",
+  ham: "turkey",
+  cheese: "swiss",
 };
 
 console.log(type_sandwich.bread);
